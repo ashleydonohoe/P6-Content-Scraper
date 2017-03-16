c17569d5a5287d8a6487a08eeac32f8f76a57519
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-03-16 00:06:45 -0400

    JSON to CSV is working

--- scraper.js
+++ scraper.js
@@ -1,15 +1,10 @@
-// Scraping and Saving Data:
-//     The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
-//     The information should be stored in an CSV file that is named for the date it was created, e.g. 2016- 11-21.csv.
-//     Assume that the the column headers in the CSV need to be in a certain order to be correctly entered into a database. They should be in this order: Title, Price, ImageURL, URL, and Time
-// The CSV file should be saved inside the ‘data’ folder.
-
 'use strict';
 const cheerio = require('cheerio');
 const request = require('request');
 const json2csv = require('json2csv');
 const fs = require('fs');
 const rootURL = "http://www.shirts4mike.com/shirts.php";
+const baseURL = "http://www.shirts4mike.com/";
 
 // Checks if the data folder exists and creates it if not
 // Used info from https://blog.raananweber.com/2015/12/15/check-if-a-directory-exists-in-node-js/
@@ -28,7 +23,6 @@ getURLs(rootURL);
 // Used this tutorial: http://www.netinstructions.com/simple-web-scraping-with-node-js-and-javascript/
 function getURLs(rootURL) {
     let links = [];
-    const baseURL = "http://www.shirts4mike.com/";
     request(rootURL, function (error, response, body) {
         if(error) {
             // Shows error that occurred
@@ -51,15 +45,54 @@ function getURLs(rootURL) {
 function getShirtInfo(links) {
     let jsonData = [];
     for(let i = 0; i < links.length; i++) {
+        const link = links[i];
+        // Date formatting tip taken from http://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd
+        const date = new Date().toISOString().split('T')[0];
         // Make request
         // Check body for:
-            // price, title, url and image url
-        // Make JSON object with properties
-        // Push JSON object to jsonData array
+            // .price
+            // title h1 (exclude .price)
+            // .shirt-picture img
+        request(link, function (error, response, body) {
+            if(error) {
+                // Shows error that occurred
+                console.log(`Could not connect to ${link} to get the data due to error: ${error}`);
+            } else {
+                var $ = cheerio.load(body);
+                const price = $(".price").text().trim();
+                // Used method to exclude span tag from http://stackoverflow.com/questions/11347779/jquery-exclude-children-from-text
+                const title = $(".shirt-details h1").clone().find('span').remove().end().text();
+                console.log(title);
+                const imageURL = baseURL + $(".shirt-picture img").attr("src");
+
+                // Make JSON object with properties
+                let newShirt = {
+                    "Title": title,
+                    "Price": price,
+                    "ImageURL": imageURL,
+                    "URL": link,
+                    "Time": date
+                };
+
+                // Push JSON object to jsonData array
+                jsonData.push(newShirt);
+            }
+            // Send request to make the CSV file
+            makeCSVFromJSON(jsonData);
+        });
     }
 }
 
 function makeCSVFromJSON(jsonData) {
+    try {
+        var result = json2csv({ data: jsonData});
+        console.log(result);
+        const date = new Date().toISOString().split('T')[0];
+        const filePath = "data/" + date + ".csv";
 
+        fs.writeFileSync(filePath, result);
+    } catch (err) {
+        console.log("Could not convert JSON to CSV");
+    }
 }
 
