ca35a36e052ff36d9e3e6906d988079a1fdd4366
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-03-15 21:23:52 -0400

    scraping is working. now attempting to pull necessary data

--- scraper.js
+++ scraper.js
@@ -3,3 +3,27 @@
 //     The information should be stored in an CSV file that is named for the date it was created, e.g. 2016- 11-21.csv.
 //     Assume that the the column headers in the CSV need to be in a certain order to be correctly entered into a database. They should be in this order: Title, Price, ImageURL, URL, and Time
 // The CSV file should be saved inside the ‘data’ folder.
+
+const scrape = require('website-scraper');
+const json2csv = require("json2csv");
+const fs = require('fs');
+
+// Setup for website-scraper
+const options = {
+    urls: ['http://www.shirts4mike.com/shirts.php'],
+    directory: 'data/'
+};
+
+scrape(options).then((result) => {
+    console.log(result);
+    pullInfo("data/index.html");
+}).catch((err) => {
+    console.log(`There's been an error: ${err}`);
+});
+
+function pullInfo(filePath) {
+    let fileContent = fs.readFileSync(filePath, {encoding: "utf8");
+    console.log(fileContent);
+
+}
+
